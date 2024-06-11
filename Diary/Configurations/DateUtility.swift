d9002eaadf89843b043dde55cae9035ea4641ede
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-06-12 00:37:16 +0900

    24/06/12 refactoring

---
+++ Diary/Configurations/DateUtility.swift
@@ -0,0 +1,8 @@
+//
+//  DateUtility.swift
+//  Diary
+//
+//  Created by 성재 on 6/11/24.
+//
+
+import Foundation
---
+++ Diary/Configurations/Utility.swift
@@ -0,0 +1,8 @@
+//
+//  Utility.swift
+//  Diary
+//
+//  Created by 성재 on 6/11/24.
+//
+
+import Foundation

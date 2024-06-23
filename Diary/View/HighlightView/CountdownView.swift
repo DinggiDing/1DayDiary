9e5aec24db3c97ef7ffd3ddc52a9b7509682425b
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-06-23 19:21:36 +0900

    24/06/23 : timer

---
+++ Diary/View/HighlightView/CountdownView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownView.swift
+//  Diary
+//
+//  Created by 성재 on 6/16/24.
+//
+
+import SwiftUI
+
+struct CountdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownView()
+}

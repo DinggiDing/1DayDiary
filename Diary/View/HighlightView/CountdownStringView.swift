12bc5c7b4f77b0eab3154932f288f173860445dc
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2024-08-06 17:12:55 +0900

    24.08.06 v1.2.0 Final

---
+++ Diary/View/HighlightView/CountdownStringView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownStringView.swift
+//  Diary
+//
+//  Created by 성재 on 8/1/24.
+//
+
+import SwiftUI
+
+struct CountdownStringView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownStringView()
+}
